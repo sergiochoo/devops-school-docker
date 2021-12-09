6298ed04927d73a38805e0510408dc74c6257ecd
Sergei Afanasenko <afanasenko.s@gmail.com>
2021-12-09 20:28:27 +0300

    back to localhost

--- frontend/src/BookApi.js
+++ frontend/src/BookApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = 'http://backend:8000/api/v1/lib/book/';
+const API_URL = 'http://localhost:8000/api/v1/lib/book/';
 
 export default class BookManager {
 
--- frontend/src/CitiesApi.js
+++ frontend/src/CitiesApi.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const API_URL = 'http://backend:8000/api/v1/lib/issue_city/';
+const API_URL = 'http://localhost:8000/api/v1/lib/issue_city/';
 
 export default class CityManager {
 
